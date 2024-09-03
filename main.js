650c956e7f34eb34d05cdc85558c1c8ba7d84075
Hua, Jie <Jie.Hua@lmu.de>
2024-09-03 13:34:33 +0200

    update

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, 'build/icon.ico'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
